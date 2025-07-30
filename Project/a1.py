a5e700faadd407b9844268014e1a76445d0b743e
nakiz0 <amritxtamu132@gmail.com>
2025-07-30 21:55:23 +0530

    matrix (linear algebra)

--- Project/a1.py
+++ Project/a1.py
@@ -1,5 +1,13 @@
 import numpy as np
-a=np.identity(5)
-deter=np.linalg.det(a)
+a = np.random.randint(1, 10, (4, 5))
+b = np.random.randint(1, 10, (5, 4))
+print("the first  matrix is:\n ",a)
+print("the second matrix is:\n ",b)
+
+mul=np.matmul(a,b)
+print("the multiplication of the two matrices is:\n")
+print(mul)   
+print("the determinant of the multiplication matrix is: ")
+deter=np.linalg.det(mul)
 
 print(deter)
